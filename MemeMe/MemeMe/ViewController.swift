73e9735e282c4c286e117655658fd2cb0633168a
tiagooliveira95 <tiago-oliveira-1995@hotmail.com>
2020-04-19 18:32:14 +0100

    Added text styles; Code improvements; Minor bug fixes

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -8,10 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    // Variable to check if the user has rewritten the default text
+    // when set to true, the text wont get cleared when user touches the text field
+    var wasTopDefaultTextOverwritten = false
+    var wasBottomDefaultTextOverwritten = false
+
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0),
+        NSAttributedString.Key.strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1),
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth:  -2
+    ]
+    
     
     //Delegate is called when image is picked by the user
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -33,13 +48,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
 
-    
     override func viewWillAppear(_ animated: Bool) {
         subscribeToKeyboardNotifications()
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
-
     
+    
+    override func viewDidLoad() {
+        setTextStyles(topTextField, "Top")
+        setTextStyles(bottomTextField, "Bottom")
+    }
+    
+    func setTextStyles(_ textField: UITextField, _ defaultText: String){
+        textField.text = defaultText
+        textField.defaultTextAttributes = memeTextAttributes
+        textField.textAlignment = NSTextAlignment.center
+        textField.delegate = self
+    }
+
     @IBAction func pickImage(_ sender: Any) {
         //Shows image picker
         let pickerController = UIImagePickerController()
@@ -58,12 +84,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
 
     @objc func keyboardWillShow(_ notification: Notification){
-        view.frame.origin.y = -getKeyboardHight(notification)
+        // Makes sure bottom TextField is editing otherwise there is no need to elevate the view
+        // because doing so will probably hide the textField that the user is typing on
+        if bottomTextField.isEditing {
+            view.frame.origin.y = -getKeyboardHight(notification)
+        }
     }
     
     func subscribeToKeyboardNotifications(){
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)),
-                                               name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default
+            .addObserver(self, selector: #selector(keyboardWillShow(_:)),
+                         name: UIResponder.keyboardWillShowNotification, object: nil)
     }
     
     func getKeyboardHight(_ notification: Notification) -> CGFloat{
@@ -71,5 +102,25 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
         return keyboardSize.cgRectValue.height
     }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField){
+        if bottomTextField.isEditing && !wasBottomDefaultTextOverwritten {
+            textField.text = nil
+            wasBottomDefaultTextOverwritten = true
+        } else if topTextField.isEditing && !wasTopDefaultTextOverwritten {
+            textField.text = nil
+            wasTopDefaultTextOverwritten = true
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        // set y back to 0 when keyboard is closed
+        view.frame.origin.y = 0
+        print(textField.text!)
+        return true
+    }
+    
+    
 }
 
