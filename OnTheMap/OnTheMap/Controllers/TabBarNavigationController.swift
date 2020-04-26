df13ce019733604c7363aad533351ce055ffbde7
tiagooliveira95 <tiago-oliveira-1995@hotmail.com>
2020-04-26 02:37:43 +0100

    Screen Views added; Added Art

---
+++ OnTheMap/OnTheMap/Controllers/TabBarNavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarNavigationController.swift
+//  OnTheMap
+//
+//  Created by Tiago Oliveira on 26/04/2020.
+//  Copyright © 2020 Tiago Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class TabBarNavigationController : UITabBarController {
+    
+}
