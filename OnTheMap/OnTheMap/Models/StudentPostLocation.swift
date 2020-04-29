c6bd6d728e13e386c015a3d29674ccda91b8ae82
tiagooliveira95 <tiago-oliveira-1995@hotmail.com>
2020-04-29 20:33:26 +0100

    Created

---
+++ OnTheMap/OnTheMap/Models/StudentPostLocation.swift
@@ -0,0 +1,19 @@
+//
+//  StudentPostLocation.swift
+//  OnTheMap
+//
+//  Created by Tiago Oliveira on 28/04/2020.
+//  Copyright © 2020 Tiago Oliveira. All rights reserved.
+//
+
+import Foundation
+
+class StudentPostLocation: Codable {
+    var uniqueKey: String!
+    var firstName: String!
+    var lastName: String!
+    var mapString: String!
+    var mediaURL: String!
+    var latitude: Double!
+    var longitude: Double!
+}
