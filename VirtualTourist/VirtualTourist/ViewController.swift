cd52570dde56fa74cd7fef4c30f3e784f7e621d9
tiagooliveira95 <tiago-oliveira-1995@hotmail.com>
2020-04-30 22:43:08 +0100

    Initial commit

---
+++ VirtualTourist/VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Tiago Oliveira on 30/04/2020.
+//  Copyright © 2020 Tiago Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
