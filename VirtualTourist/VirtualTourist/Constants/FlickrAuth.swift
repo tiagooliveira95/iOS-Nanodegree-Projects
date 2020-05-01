748f6bd0b99042654ca066ecc5ca270cdb120253
tiagooliveira95 <tiago-oliveira-1995@hotmail.com>
2020-05-02 00:36:13 +0100

    Created

---
+++ VirtualTourist/VirtualTourist/Constants/FlickrAuth.swift
@@ -0,0 +1,12 @@
+//
+//  ProviderConstants.swift
+//  VirtualTourist
+//
+//  Created by Tiago Oliveira on 02/05/2020.
+//  Copyright © 2020 Tiago Oliveira. All rights reserved.
+//
+
+class FlickrAuth{
+    static let API_KEY = ""
+    static let API_SECRET = ""
+}
