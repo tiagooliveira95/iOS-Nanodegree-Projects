df90947704998aa41f3518a70ad250a1cbf198be
tiagooliveira95 <tiago-oliveira-1995@hotmail.com>
2020-05-01 03:11:22 +0100

    Created: PhotoAlbumAdded

---
+++ VirtualTourist/VirtualTourist/Controllers/PhotoAlbumViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Tiago Oliveira on 01/05/2020.
+//  Copyright © 2020 Tiago Oliveira. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PhotoAlbumViewController: UIViewController{
+    
+    @IBOutlet weak var noImagesLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+}
