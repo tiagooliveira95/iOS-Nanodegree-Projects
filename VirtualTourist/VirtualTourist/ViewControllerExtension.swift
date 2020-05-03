7e1215f427ee3b6a4e5c68f7a03b64e7058982b4
tiagooliveira95 <tiago-oliveira-1995@hotmail.com>
2020-05-03 23:22:43 +0100

    Created

---
+++ VirtualTourist/VirtualTourist/ViewControllerExtension.swift
@@ -0,0 +1,41 @@
+//
+//  ViewControllerExtension.swift
+//  VirtualTourist
+//
+//  Created by Tiago Oliveira on 03/05/2020.
+//  Copyright © 2020 Tiago Oliveira. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func showUIAlert(title: String, message: String?, style: UIAlertController.Style, actions: [UIAlertAction], viewController: UIViewController?){
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+            
+            if actions.count == 0 {
+                let closeAction = UIAlertAction(title: "Close", style: .default, handler: nil)
+                alert.addAction(closeAction)
+            } else {
+                for action in actions {
+                    alert.addAction(action)
+                }
+            }
+            self.present(alert, animated: true)
+        }
+    }
+    
+    func showActivityLoadingIndicatorView(_ message: String? = "Please wait a moment...") {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.style = UIActivityIndicatorView.Style.gray
+        loadingIndicator.startAnimating();
+
+        alert.view.addSubview(loadingIndicator)
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
