00ccc0ab8b533590b6283535acc4c931299a5312
tiagooliveira95 <tiago-oliveira-1995@hotmail.com>
2020-05-05 02:48:02 +0100

    Initial Commit

---
+++ Shopping List/Shopping List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shopping List
+//
+//  Created by Tiago Oliveira on 05/05/2020.
+//  Copyright © 2020 Tiago Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
