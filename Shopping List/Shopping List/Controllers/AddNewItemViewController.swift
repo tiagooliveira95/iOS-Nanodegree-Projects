78e14852bc69a332e50a99fb245e2b1e8ada4d86
tiagooliveira95 <tiago-oliveira-1995@hotmail.com>
2020-05-07 01:40:04 +0100

    AddNewItem Implemented

---
+++ Shopping List/Shopping List/Controllers/AddNewItemViewController.swift
@@ -0,0 +1,54 @@
+//
+//  AddNewItemViewController.swift
+//  Shopping List
+//
+//  Created by Tiago Oliveira on 06/05/2020.
+//  Copyright © 2020 Tiago Oliveira. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import Firebase
+
+class AddNewItemViewController: UIViewController{
+        
+    let coreData = (UIApplication.shared.delegate as! AppDelegate)
+    var ref: DatabaseReference!
+
+    @IBOutlet weak var itemTextField: UITextField!
+    @IBOutlet weak var amountTextField: UITextField!
+
+    override func viewWillAppear(_ animated: Bool) {
+        ref = Database.database().reference()
+    }
+    
+    @IBAction func addButtonClicked(_ sender: Any) {
+        let userFetch:NSFetchRequest = User.fetchRequest()
+        let user = try! self.coreData.persistentContainer.viewContext.fetch(userFetch).first
+        
+        self.ref.child("family/\(user!.family!)/items/").child(generateItemUID()).setValue(
+                [
+                    "name" : itemTextField.text!,
+                    "amount": Int(amountTextField.text!) ?? 0
+                ]
+        ) { (error, ref) -> Void in
+            guard error != nil else{
+                //todo show error0
+                return
+            }
+            self.dismiss(animated: true)
+        }
+    }
+    
+    func generateItemUID()->String{
+        var uid = ""
+        let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+        let maxRandom = chars.count
+        
+        for _ in 0...20 {
+            uid += String(chars[chars.index(chars.startIndex, offsetBy: Int.random(in: 0...maxRandom))])
+        }
+        return uid
+    }
+    
+}
